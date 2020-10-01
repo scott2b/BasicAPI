ffe47f85f7ef53a810d375c618d0fd9138909cfa
Scott Bradley <scottbradley.mail@gmail.com>
2020-10-01 15:08:52 -0500

    replace dashboard with correct code

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div>
-    <notifications></notifications>
-    <router-view :key="$route.fullPath"></router-view>
+  <div id="app">
+    <v-app>
+      <v-content v-if="loggedIn===null">
+        <v-container fill-height>
+          <v-layout align-center justify-center>
+            <v-flex>
+              <div class="text-xs-center">
+                <div class="headline my-5">Loading...</div>
+                <v-progress-circular size="100" indeterminate color="primary"></v-progress-circular>
+              </div>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-content>
+      <router-view v-else />
+      <NotificationsManager></NotificationsManager>
+    </v-app>
   </div>
 </template>
 
-<script>
-  export default {
-    methods: {
-      disableRTL() {
-        if (!this.$rtl.isRTL) {
-          this.$rtl.disableRTL();
-        }
-      },
-      toggleNavOpen() {
-        let root = document.getElementsByTagName('html')[0];
-        root.classList.toggle('nav-open');
-      }
-    },
-    mounted() {
-      this.$watch('$route', this.disableRTL, { immediate: true });
-      this.$watch('$sidebar.showSidebar', this.toggleNavOpen)
-    }
-  };
-</script>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import NotificationsManager from '@/components/NotificationsManager.vue';
+import { readIsLoggedIn } from '@/store/main/getters';
+import { dispatchCheckLoggedIn } from '@/store/main/actions';
 
-<style lang="scss"></style>
+@Component({
+  components: {
+    NotificationsManager,
+  },
+})
+export default class App extends Vue {
+
+  get loggedIn() {
+    return readIsLoggedIn(this.$store);
+  }
+
+  public async created() {
+    await dispatchCheckLoggedIn(this.$store);
+  }
+}
+</script>
--- dashboard/vue.config.js
+++ dashboard/vue.config.js
@@ -1,37 +1,35 @@
-const webpack = require('webpack');
-
 module.exports = {
-  lintOnSave: false,
-  configureWebpack: {
-    // Set up all the aliases we use in our app.
-    resolve: {
-      alias: {
-        'chart.js': 'chart.js/dist/Chart.js'
-      }
-    },
-    plugins: [
-      new webpack.optimize.LimitChunkCountPlugin({
-        maxChunks: 6
-      })
-    ]
-  },
-  pwa: {
-    name: 'Vue Black Dashboard',
-    themeColor: '#344675',
-    msTileColor: '#344675',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: '#344675'
-  },
-  pluginOptions: {
-    i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: false
+  // Fix Vuex-typescript in prod: https://github.com/istrib/vuex-typescript/issues/13#issuecomment-409869231
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.optimization.minimizer[0].options.terserOptions = Object.assign(
+        {},
+        config.optimization.minimizer[0].options.terserOptions,
+        {
+          ecma: 5,
+          compress: {
+            keep_fnames: true,
+          },
+          warnings: false,
+          mangle: {
+            keep_fnames: true,
+          },
+        },
+      );
     }
   },
-  css: {
-    // Enable CSS source maps.
-    sourceMap: process.env.NODE_ENV !== 'production'
-  }
-};
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => Object.assign(options, {
+        transformAssetUrls: {
+          'v-img': ['src', 'lazy-src'],
+          'v-card': 'src',
+          'v-card-media': 'src',
+          'v-responsive': 'src',
+        }
+      }));
+  },
+}
